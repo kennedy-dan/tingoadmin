66c42c66fa07a3f15abb66c4de9042c7b3899332
kennedy-dan <kenidankid@yahoo.com>
2024-08-23 11:12:44 +0100

    router

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -5,18 +5,23 @@ import '~/public/fonts/Linearicons/Font/demo-files/demo.css';
 import '~/public/fonts/font-awesome/css/font-awesome.css';
 import 'public/css/bootstrap.min.css';
 import '~/styles/style.scss';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/navigation';
 export const metadata = {
-    title: 'Martfury - Multipurpose Marketplace React Ecommerce Template',
+    // title: 'Martfury - Multipurpose Marketplace React Ecommerce Template',
     description: '',
 }
 
 export default function Template({ children }) {
-    // useEffect(() => {
-    //     setTimeout(function() {
-    //         document.getElementById('__next').classList.add('loaded');
-    //     }, 100);
-    // }, []);
+
+    const dispatch = useDispatch()
+    const router = useRouter();
+    const {user} = useSelector(state => state.auth)
+    useEffect(() => {
+      if(!user){
+        router.push('/account/login')
+      }
+    }, []);
 
     return (
         <div>
