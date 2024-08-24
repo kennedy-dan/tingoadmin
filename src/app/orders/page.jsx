e144b07050fa944b9e63a9f661bc304f7fa96793
kennedy-dan <kenidankid@yahoo.com>
2024-08-24 13:23:11 +0100

    eect

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, {useEffect} from 'react';
 import ContainerDefault from '~/components/layouts/ContainerDefault';
 import TableOrdersItems from '~/components/shared/tables/TableOrdersItems';
 import Pagination from '~/components/elements/basic/Pagination';
